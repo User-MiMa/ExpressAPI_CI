859f20c8ade8d9ebe9de2c8df55909674e40383c
User-MiMa <mlsmasson@outlook.com>
2026-09-06 07:32:34 -0600

    refactor: add explicit HOST

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { app } from './app.js';
 
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log(`Server listening on port ${port}`);
 });
